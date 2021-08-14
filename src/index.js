ef8bee9f6c913b418be506bcb56967da9a7dd888
Erick Danzer <edanzer@gmail.com>
2021-08-13 22:07:32 -0600

    Add wp-script js and css entry points, package-json commands

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/*
+ *
+ * Following @wordpress/scripts standards, this src/index.js file is the entry
+ * point for all JS and CSS processing. The resulting compile JS file will be 
+ * at build/index.js.
+ * 
+ * This JS file also looks for and generates two CSS files. The index.scss file
+ * and all related styles will become build/index.css. This is for block editor-only css. 
+ * The style.scss file and all connected styles will become style.css and is for 
+ * style applying to both front end and block editor. 
+ * 
+ */
+
+/* 
+ * Import CSS 
+ * 
+ * Again following @wordpress/scripts standards, we import and generates two CSS files. 
+ * The index.scss file and all related styles will become build/index.css. This is for 
+ * block editor-only css. The style.scss file and all connected styles will become 
+ * style.css and is for style applying to both front end and block editor. 
+ * 
+ */
+import './index.scss';
+import './style.css';
+
+/*
+ * Import JS
+ */
